092f4ac8cc33221f55ee0a23d92426947b8c0dad
VladTrofymyshyn <vladtrofymyshyn@gmail.com>
2023-11-30 17:30:43 +0200

    Достав інфу про студентів

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 class Group_1012:
-    pass
-student1= Group_1012()
-student2= Group_1012()
-student3= Group_1012()
-student4= Group_1012()
-student5= Group_1012()
+    def __init__(self, name,gen,age,evarage_mark):# ініціалізація атрибутів класу
+        self.name=name
+        self.gen=gen
+        self.age=age
+        self.evarage_mark= evarage_mark
+
+student1= Group_1012("Гончарук Олександр", "чол.", 12, 12)#об'єкт тобто екземпляр класу
+student2= Group_1012("Трофимишин Владислав", "чол", 12,12)
+student3= Group_1012("Радомська Дарина",  "жін.",  12,  13)
+student4= Group_1012("Коваленко Макар", "чол", 13, 11.1)
+student5= Group_1012("Юраш Максим","чол.", 14, 10)
 
